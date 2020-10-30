06848b1cb871d793d530c4df13a5fa13f90b5f67
unknown <lavanya1218.g@gmail.com>
2020-10-30 16:16:56 -0600

    Final Version

--- Program.cs
+++ Program.cs
@@ -13,18 +13,23 @@ namespace Quiz1
         }
         public static void TP(double a)
         {
-            double TktPrice =0;
+            double TktPrice;
            
             if (a<0 || a>130)
             {
                 Console.WriteLine("Please enter valid age");
+                
                 return;
+            }
+             else if(a>=0 && a<=5)
+            {
+                TktPrice = 0;
             }
            else if(a>5 && a<=14)
             {
                 TktPrice = 7.99;
             }
-            else if(a>14 && a<=65)
+            else if(a>14 && a<=64)
             {
                 TktPrice = 11.99;
             }
@@ -32,7 +37,17 @@ namespace Quiz1
             {
                 TktPrice = 9.99;
             }
-            Console.WriteLine("Price of movie ticket is " + TktPrice);
+            DateTime currentdate = DateTime.Now;
+            if(currentdate.DayOfWeek == DayOfWeek.Tuesday){           
+                TktPrice/=2;
+            }
+             Console.WriteLine("Do you have a student ID card? Yes or No");
+             String StudentId = Console.ReadLine();
+             if(StudentId == "Yes"||StudentId == "yes"|| StudentId == "YES")
+             {
+                 TktPrice-=1;
+             }
+            Console.WriteLine("Price of movie ticket is " + (Math.Round(TktPrice,2)));
         }
 
     }
